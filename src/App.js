8b4b80ca1919f4e0ecd789e5c717d7d5cda7388e
Merve Barış <mrvbaris1@gmail.com>
2022-12-18 22:48:15 +0300

    init

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
       CompletedOn: CompletedOn,
     };
 
-    let updatedCompletedArr = [...completedTodos, filteredItem];
+    let updatedCompletedArr = [...completedTodos];
     updatedCompletedArr.push(filteredItem);
     setCompletedTodos(updatedCompletedArr);
     localStorage.setItem("completedTodos", JSON.stringify(updatedCompletedArr));
